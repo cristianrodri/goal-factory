a14f482374ecaf967e3256be17459fdfd8ed4ef9
Cristian <contacto.cristianrodriguez@gmail.com>
2024-01-12 12:58:59 -0300

    fix: msw handlers are deleted

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,1 @@
 import '@testing-library/jest-dom'
-import 'whatwg-fetch'
-
-import { server } from './src/test-setup/server'
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
--- src/test-setup/server-handlers.ts
+++
@@ -1,9 +0,0 @@
-import { HttpResponse, http } from 'msw'
-
-const handlers = [
-  http.post('/api/user/signup', () => {
-    return HttpResponse.json({ success: true })
-  })
-]
-
-export { handlers }
