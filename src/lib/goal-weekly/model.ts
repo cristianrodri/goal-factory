0779515cdca162ae1eaeac745f5d4350e07ce7ca
Cristian <contacto.cristianrodriguez@gmail.com>
2024-01-29 18:47:08 -0300

    feat: goal weekly model is added

---
+++ src/lib/goal-weekly/model.ts
@@ -0,0 +1,44 @@
+import { IGoalWeekly } from '@/types'
+import { Model, Schema, model, models } from 'mongoose'
+
+const goalWeeklySchema = new Schema<IGoalWeekly>({
+  goals: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Goal'
+    }
+  ],
+  weeklyProgress: {
+    type: Number,
+    default: 0
+  },
+  achievements: [
+    {
+      type: String,
+      minlength: [3, 'Achievement must be at least 3 characters long'],
+      maxlength: [100, 'Achievement must be at most 100 characters long']
+    }
+  ],
+  reward: {
+    type: String,
+    required: [true, 'Reward is required'],
+    minlength: [3, 'Reward must be at least 3 characters long'],
+    maxlength: [50, 'Reward must be at most 100 characters long']
+  },
+  goal: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Goal'
+  },
+  aspiration: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Aspiration'
+  }
+})
+
+const GoalWeekly =
+  (models['GoalWeekly'] as Model<IGoalWeekly>) ||
+  model<IGoalWeekly>('GoalWeekly', goalWeeklySchema)
+
+export default GoalWeekly
