8dc021023050cbc309046335d9b1fe87d963580f
Cristian <contacto.cristianrodriguez@gmail.com>
2024-01-29 19:24:59 -0300

    fix: get the user id in the argument of the privateApi funciont instead of external class file

--- src/app/api/user/get/route.ts
+++ src/app/api/user/get/route.ts
@@ -1,11 +1,10 @@
 import User from '@/lib/user/model'
 import { privateApi } from '@/utils/api'
-import { user } from '@/utils/classes/User'
 import { NextResponse } from 'next/server'
 
-export const GET = privateApi(async () => {
+export const GET = privateApi(async userId => {
   // Add the user id to the class
-  const data = await User.findById(user.id)
+  const data = await User.findById(userId)
 
   return NextResponse.json(data)
 })
