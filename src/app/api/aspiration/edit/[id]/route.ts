a5cb24350a0d74350c3f0071b156834203a1db8e
Cristian <contacto.cristianrodriguez@gmail.com>
2024-02-05 19:38:47 -0300

    feat: edit aspiration api is added

---
+++ src/app/api/aspiration/edit/[id]/route.ts
@@ -0,0 +1,22 @@
+import Aspiration from '@/lib/aspiration/model'
+import { IAspiration } from '@/types'
+import { privateApi } from '@/utils/api'
+import { Status } from '@/utils/enums'
+import { NextResponse } from 'next/server'
+
+export const POST = privateApi<Omit<IAspiration, 'user'>, { id: string }>(
+  async (userId, { body, params }) => {
+    const aspiration = await Aspiration.findOneAndUpdate(
+      { user: userId, _id: params.id },
+      {
+        ...body,
+        user: userId
+      },
+      { new: true }
+    )
+
+    return NextResponse.json(aspiration, {
+      status: Status.CREATED
+    })
+  }
+)
