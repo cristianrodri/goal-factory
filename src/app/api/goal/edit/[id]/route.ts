bd20ca603269ed427bd9db132dac80ab4697739a
Cristian <contacto.cristianrodriguez@gmail.com>
2024-02-06 19:45:59 -0300

    feat: goal edit api is added

---
+++ src/app/api/goal/edit/[id]/route.ts
@@ -0,0 +1,28 @@
+import Goal from '@/lib/goal/model'
+import { IGoal } from '@/types'
+import { privateApi } from '@/utils/api'
+import { Status } from '@/utils/enums'
+import { NextResponse } from 'next/server'
+
+export const PUT = privateApi<Omit<IGoal, 'user'>, { id: string }>(
+  async (userId, { body, params }) => {
+    const { aspiration } = body
+
+    if (aspiration) {
+      return NextResponse.json(
+        { error: 'Aspiration cannot be updated' },
+        { status: Status.BAD_REQUEST }
+      )
+    }
+
+    const goal = await Goal.findOneAndUpdate(
+      { user: userId, _id: params.id },
+      body,
+      { new: true }
+    )
+
+    return NextResponse.json(goal, {
+      status: Status.CREATED
+    })
+  }
+)
