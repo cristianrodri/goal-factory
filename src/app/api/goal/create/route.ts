b82f004d4f1a309b94161b8639c4e9a5a8aca9e2
Cristian <contacto.cristianrodriguez@gmail.com>
2024-02-12 18:55:35 -0300

    fix: goals are added correctly by getting an array of objects as a body and checking if the given aspiration id exists or belongs to the user

--- src/app/api/goal/create/route.ts
+++ src/app/api/goal/create/route.ts
@@ -1,20 +1,52 @@
+import Aspiration from '@/lib/aspiration/model'
 import Goal from '@/lib/goal/model'
-import { IAspiration } from '@/types'
+import { IGoal } from '@/types'
 import { privateApi } from '@/utils/api'
 import { Status } from '@/utils/enums'
 import { NextResponse } from 'next/server'
 
-export const POST = privateApi<Omit<IAspiration, 'user'>>(
+export const POST = privateApi<Omit<IGoal[], 'user'>>(
   async (userId, { body }) => {
-    const goal = new Goal({
-      ...body,
-      weeklyProgress: 0,
+    const areGoalsSharingSameAspiration = body.every(
+      goal => goal.aspiration === body[0].aspiration
+    )
+
+    if (!areGoalsSharingSameAspiration) {
+      return NextResponse.json(
+        {
+          message: 'All goals must share the same aspiration'
+        },
+        {
+          status: Status.BAD_REQUEST
+        }
+      )
+    }
+
+    // Check if the given aspiration exists and belongs to the user
+    const aspirationExists = await Aspiration.exists({
+      _id: body[0].aspiration,
       user: userId
     })
 
-    const createdGoal = await goal.save()
+    if (!aspirationExists) {
+      return NextResponse.json(
+        {
+          message: 'Aspiration does not exist or does not belong to the user'
+        },
+        {
+          status: Status.BAD_REQUEST
+        }
+      )
+    }
 
-    return NextResponse.json(createdGoal, {
+    // Add the user id to each goal
+    const newGoals = body.map(goal => ({ ...goal, user: userId }))
+
+    const goal = new Goal(newGoals)
+
+    const createdGoals = await goal.save()
+
+    return NextResponse.json(createdGoals, {
       status: Status.CREATED
     })
   }
