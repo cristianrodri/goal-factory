1276e80f261cb5ba06af6e420bcecbc147f2473e
Cristian <contacto.cristianrodriguez@gmail.com>
2024-05-14 19:24:16 -0300

    refactor: BigGoal model is updated

--- src/lib/big-goal/model.ts
+++ src/lib/big-goal/model.ts
@@ -10,42 +10,114 @@ const bigGoalSchema = new Schema<IBigGoal>({
     type: String,
     required: [true, 'General result is required'],
     trim: true,
-    maxlength: 100,
-    minlength: 2
+    maxlength: [500, 'General result can not be more than 500 characters'],
+    minlength: [2, 'General result can not be less than 2 characters']
   },
   specificResult: {
     type: String,
     required: [true, 'Specific result is required'],
     trim: true,
-    maxlength: 100,
-    minlength: 2
+    maxlength: [500, 'Specific result can not be more than 500 characters'],
+    minlength: [2, 'Specific result can not be less than 2 characters']
   },
-  // deadline: {
-  //   type: Date,
-  //   required: [true, 'Deadline is required'],
-  //   validate: {
-  //     validator: function (value: Date) {
-  //       // Check if the provided date is greater than or equal to the current day
-  //       return value >= new Date()
-  //     },
-  //     message: 'Deadline must be greater than or equal to the current day'
-  //   }
-  // },
-  // reached: {
-  //   type: Boolean,
-  //   default: false
-  // },
+  realDeadline: {
+    type: Date,
+    validate: {
+      validator: function (value: Date) {
+        // Check if the provided date is greater than or equal to the current day
+        return value >= new Date()
+      },
+      message: 'Real Deadline must be greater than or equal to the current day'
+    }
+  },
+  optimisticDeadline: {
+    type: Date,
+    required: [true, 'Deadline is required'],
+    validate: {
+      validator: function (value: Date) {
+        // Check if the provided date is greater than or equal to the current day
+        return value >= new Date()
+      },
+      message: 'Deadline must be greater than or equal to the current day'
+    }
+  },
+  activityAnalysis: [
+    {
+      type: String,
+      trim: true,
+      maxlength: [300, 'Activity analysis can not be more than 300 characters'],
+      minlength: [2, 'Activity analysis can not be less than 2 characters']
+    }
+  ],
+  activitiesFrecuency: {
+    type: String,
+    trim: true,
+    maxlength: [
+      1000,
+      'Activities frecuency can not be more than 1000 characters'
+    ],
+    minlength: [2, 'Activities frecuency can not be less than 2 characters']
+  },
+  bigWhy: {
+    type: String,
+    trim: true,
+    maxlength: [700, 'Big why can not be more than 700 characters'],
+    minlength: [2, 'Big why can not be less than 2 characters']
+  },
+  expectation: {
+    type: String,
+    trim: true,
+    maxlength: [700, 'Expectation can not be more than 700 characters'],
+    minlength: [2, 'Expectation can not be less than 2 characters']
+  },
+  compromises: {
+    type: Map,
+    of: Boolean
+  },
+  moderatingFactors: {
+    type: Map,
+    of: [String]
+  },
+  moderationFactorAlternatives: [
+    {
+      type: String,
+      trim: true,
+      maxlength: [
+        300,
+        'Moderation factor alternatives can not be more than 300 characters'
+      ],
+      minlength: [
+        2,
+        'Moderation factor alternatives can not be less than 2 characters'
+      ]
+    }
+  ],
+  facilitators: {
+    type: Map,
+    of: [String]
+  },
+  basicAspects: {
+    type: Map,
+    of: Boolean
+  },
+  optimizingAspects: {
+    type: Map,
+    of: Boolean
+  },
   bigReward: {
     type: String,
-    required: [true, 'Big reward is required'],
     trim: true,
-    maxlength: 100,
-    minlength: 2
+    maxlength: [200, 'Big reward can not be more than 200 characters'],
+    minlength: [2, 'Big reward can not be less than 2 characters']
   },
-  // progress: {
-  //   type: Number,
-  //   default: 0
-  // },
+  rewardWasTaken: {
+    type: Boolean,
+    default: false
+  },
+  achieved: {
+    type: Boolean,
+    default: false
+  },
   user: {
     type: Schema.Types.ObjectId,
     required: true,
@@ -56,8 +128,8 @@ const bigGoalSchema = new Schema<IBigGoal>({
 // Define a virtual property to populate the associated goal
 bigGoalSchema.virtual('goals', {
   ref: 'Goal', // Reference the Goal model
-  localField: '_id', // Field from the Aspiration model
-  foreignField: 'aspiration' // Field from the Goal model
+  localField: '_id', // Field from the BigGoal model
+  foreignField: 'bigGoal' // Field from the Goal model
 })
 
 bigGoalSchema.pre('findOneAndDelete', async function () {
