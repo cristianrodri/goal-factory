311b0be22dd88a4e06ecf51fabae407255e3a92d
Cristian <contacto.cristianrodriguez@gmail.com>
2024-05-14 19:13:54 -0300

    refactor: bigGoal types are updated

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -53,6 +53,25 @@ interface IBigGoal {
     3: string[]
     4: string
   }
+  basicAspects: {
+    1: boolean
+    2: boolean
+    3: boolean
+    4: boolean
+    5: boolean
+  }
+  optimizingAspects: {
+    1: boolean
+    2: boolean
+    3: boolean
+    4: boolean
+    5: boolean
+    6: boolean
+    7: boolean
+    8: boolean
+    9: boolean
+    10: boolean
+  }
   bigReward: string
   rewardWasTaken: boolean
   achieved: boolean
