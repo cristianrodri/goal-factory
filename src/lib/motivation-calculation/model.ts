1dcf82c91aefb34cb5bc246d55426840225ea912
Cristian <contacto.cristianrodriguez@gmail.com>
2024-05-22 16:27:53 -0300

    feat: uniqueness is added with user and bigGoal in motivation calculation schema

--- src/lib/motivation-calculation/model.ts
+++ src/lib/motivation-calculation/model.ts
@@ -6,26 +6,26 @@ const motivationCalculation = new Schema<IMotivationCalculation>({
   expectation: {
     type: Number,
     required: [true, 'Expectation is required'],
-    min: 1,
-    max: 10
+    min: [1, 'Expectation must be at least 1'],
+    max: [10, 'Expectation must be at most 10']
   },
   value: {
     type: Number,
     required: [true, 'Value is required'],
-    min: 1,
-    max: 10
+    min: [1, 'Value must be at least 1'],
+    max: [10, 'Value must be at most 10']
   },
   impulsiveness: {
     type: Number,
     required: [true, 'Impulsiveness is required'],
-    min: 1,
-    max: 10
+    min: [1, 'Impulsiveness must be at least 1'],
+    max: [10, 'Impulsiveness must be at most 10']
   },
   time: {
     type: Number,
     required: [true, 'Time is required'],
-    min: 1,
-    max: 10
+    min: [1, 'Time must be at least 1'],
+    max: [10, 'Time must be at most 10']
   },
   user: {
     type: Schema.Types.ObjectId,
@@ -38,6 +38,8 @@ const motivationCalculation = new Schema<IMotivationCalculation>({
     ref: 'BigGoal'
   }
 })
+
+motivationCalculation.index({ user: 1, bigGoal: 1 }, { unique: true })
 
 // Use the transformation function within the toJSON method
 motivationCalculation.methods.toJSON = function () {
