735728cd2ad061659af717888872a200bd24bd99
Cristian <contacto.cristianrodriguez@gmail.com>
2024-05-24 16:30:36 -0300

    fix: 201 status is sent when habit is created

--- src/app/api/habit/add/route.ts
+++ src/app/api/habit/add/route.ts
@@ -1,10 +1,11 @@
 import Habit from '@/lib/habit/model'
 import { IHabit } from '@/types'
 import { privateApi } from '@/utils/api'
+import { Status } from '@/utils/enums'
 import { successResponse } from '@/utils/response'
 
 export const POST = privateApi<IHabit>(async (user, { body }) => {
   const habit = await Habit.create({ ...body, user })
 
-  return successResponse(habit)
+  return successResponse(habit, Status.CREATED)
 })
