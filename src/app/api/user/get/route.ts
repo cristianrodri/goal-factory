d4420793799e22a6039d9db1f4d45e7afcf91341
Cristian <contacto.cristianrodriguez@gmail.com>
2024-06-04 19:57:52 -0300

    refactor: user get route is got by findOneOrThrow

--- src/app/api/user/get/route.ts
+++ src/app/api/user/get/route.ts
@@ -1,14 +1,9 @@
 import User from '@/lib/user/model'
 import { privateApi } from '@/utils/api'
-import { Status } from '@/utils/enums'
-import { errorResponse, successResponse } from '@/utils/response'
+import { successResponse } from '@/utils/response'
 
 export const GET = privateApi(async userId => {
-  const user = await User.findById(userId)
-
-  if (!user) {
-    return errorResponse('User not found', Status.NOT_FOUND)
-  }
+  const user = await User.findOneOrThrow({ _id: userId })
 
   return successResponse(user)
 })
