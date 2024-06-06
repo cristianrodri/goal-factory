610727487f33276e37a4bece2534f816685b715c
Cristian <contacto.cristianrodriguez@gmail.com>
2024-06-06 20:32:21 -0300

    fix: transform the _id to string when jwt is created

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,12 +1,12 @@
-import { Types } from 'mongoose'
+import { Schema } from 'mongoose'
 import jwt from 'jsonwebtoken'
 
 type TokenResponse = {
   _id: string
 }
 
-export const createJWT = (userId: { _id: Types.ObjectId }) =>
-  jwt.sign({ _id: userId._id.toString() }, process.env.JWT_KEY ?? '')
+export const createJWT = (userId: Schema.Types.ObjectId) =>
+  jwt.sign({ _id: userId.toString() }, process.env.JWT_KEY ?? '')
 
 export const getJWT = (cookieToken: string) => {
   const token = jwt.verify(
