b6c857bd3df8761139d97f446d9063dcc427c6fc
Cristian <contacto.cristianrodriguez@gmail.com>
2024-06-10 18:55:15 -0300

    fix: optimistic level model is updated by adding base schema

--- src/lib/motivation-techniques/optimistic-level/model.ts
+++ src/lib/motivation-techniques/optimistic-level/model.ts
@@ -1,8 +1,21 @@
+import {
+  findOneAndDeleteOrThrow,
+  findOneAndUpdateOrThrow,
+  findOneOrThrow,
+  IBaseDocument,
+  IBaseModel
+} from '@/lib/baseSchema'
 import { IOptimisticLevel } from '@/types'
 import { toJSONTransform } from '@/utils/db'
-import { Document, Model, Schema, model, models } from 'mongoose'
+import { Document, Schema, model, models } from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
 
-const optimisticLevelSchema = new Schema<IOptimisticLevel>({
+// Define your main interruption stimulus schema
+interface IOptimisticLevelDocument extends IOptimisticLevel, IBaseDocument {}
+
+interface IOptimisticLevelModel extends IBaseModel<IOptimisticLevelDocument> {}
+
+const optimisticLevelSchema = new Schema<IOptimisticLevelDocument>({
   improvements: [
     {
       type: String,
@@ -23,13 +36,33 @@ const optimisticLevelSchema = new Schema<IOptimisticLevel>({
   }
 })
 
+// Index the user and big goal fields
+optimisticLevelSchema.index({ user: 1, bigGoal: 1 }, { unique: true })
+
+optimisticLevelSchema.plugin(uniqueValidator, {
+  message: 'Optimistic level already exists for this big goal.'
+})
+
+// Add static method directly to schema
+optimisticLevelSchema.statics.findOneOrThrow =
+  findOneOrThrow as IBaseModel<IOptimisticLevelDocument>['findOneOrThrow']
+
+optimisticLevelSchema.statics.findOneAndUpdateOrThrow =
+  findOneAndUpdateOrThrow as IBaseModel<IOptimisticLevelDocument>['findOneAndUpdateOrThrow']
+
+optimisticLevelSchema.statics.findOneAndDeleteOrThrow =
+  findOneAndDeleteOrThrow as IBaseModel<IOptimisticLevelDocument>['findOneAndDeleteOrThrow']
+
 // Use the transformation function within the toJSON method
 optimisticLevelSchema.methods.toJSON = function () {
   return toJSONTransform(this as Document)
 }
 
 const OptimisticLevel =
-  (models['OptimisticLevel'] as Model<IOptimisticLevel>) ||
-  model<IOptimisticLevel>('OptimisticLevel', optimisticLevelSchema)
+  (models['OptimisticLevel'] as IOptimisticLevelModel) ||
+  model<IOptimisticLevelDocument, IOptimisticLevelModel>(
+    'OptimisticLevel',
+    optimisticLevelSchema
+  )
 
 export default OptimisticLevel
