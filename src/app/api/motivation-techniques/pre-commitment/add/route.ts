0232e62919a39e1e6eb36955caec2601bdbdba1f
Cristian <contacto.cristianrodriguez@gmail.com>
2024-06-11 21:06:56 -0300

    feat: add precommitment route is added

---
+++ src/app/api/motivation-techniques/pre-commitment/add/route.ts
@@ -0,0 +1,30 @@
+import PreCommitment from '@/lib/motivation-techniques/pre-commitment/model'
+import { ICommitment } from '@/types'
+import { privateApi } from '@/utils/api'
+import { Status } from '@/utils/enums'
+import { errorResponse, successResponse } from '@/utils/response'
+
+interface RequestBody {
+  bigGoal: string
+  commitment: string
+}
+
+export const POST = privateApi<RequestBody>(async (user, { body }) => {
+  const { bigGoal, commitment } = body
+
+  const preCommitment = await PreCommitment.findOneOrThrow({ user, bigGoal })
+
+  const foundSameCommitment = preCommitment.preCommitments.some(
+    p => p.commitment === commitment
+  )
+
+  if (foundSameCommitment) {
+    return errorResponse('Pre-commitment already exists', Status.CONFLICT)
+  }
+
+  preCommitment.preCommitments.push({ commitment } as ICommitment)
+
+  await preCommitment.save()
+
+  return successResponse(preCommitment, Status.CREATED)
+})
