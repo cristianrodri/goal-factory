7d7fc38c5423ff5753086986c43972c4ccdc710d
Cristian <contacto.cristianrodriguez@gmail.com>
2024-06-20 14:00:20 -0300

    feat: delete energyLevel or conclusion in delete optimized energy route

--- src/app/api/motivation-techniques/optimized-energy/delete/[id]/route.ts
+++ src/app/api/motivation-techniques/optimized-energy/delete/[id]/route.ts
@@ -1,14 +1,18 @@
 import OptimizedEnergy from '@/lib/motivation-techniques/optimized-energy/model'
 import { privateApi } from '@/utils/api'
 import { successResponse } from '@/utils/response'
+import { getParam } from '@/utils/url'
 
 export const DELETE = privateApi<unknown, { id: string }>(
-  async (user, { params }) => {
+  async (user, { params, req }) => {
+    // Property must be 'energyLevels' or 'conclusions'
+    const property = getParam(req, 'property') as string
+
     // Delete energy level by id
     const optimizedEnergy = await OptimizedEnergy.findOneAndUpdateOrThrow(
-      { user, 'energyLevels._id': params.id },
+      { user, [`${property}._id`]: params.id },
       {
-        $pull: { energyLevels: { _id: params.id } }
+        $pull: { [property]: { _id: params.id } }
       }
     )
 
